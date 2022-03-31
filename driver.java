1f6efc2365366ddf01757ea5211140829aef6cf7
Nikhil Narayanan <nnarayanan@NN.local>
2022-03-31 15:34:34 +0400

    updated

--- driver.java
+++ driver.java
@@ -12,10 +12,10 @@ public class driver {
 			// base case 
 			if (n == 1) { 
 				return 1; 
-			}
-			
-			else {
-				return n*factorial(n-1);
+				
+			} 
+			else { 
+				return n*factorial(n-1); 
 			}
 		}
 
